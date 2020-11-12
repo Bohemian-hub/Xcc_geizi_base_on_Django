64f6920313850ac4e0ee4347adb7065a82b33dfa
Bohemian-hub <dongshangwl@gmail.com>
2020-11-12 11:11:27 +0800

    创建了模板

--- xcc_gezi_project/polls/views.py
+++ xcc_gezi_project/polls/views.py
@@ -1,20 +1,21 @@
-from django.http import HttpResponse  # 可以返回渲染的页面
-from django.template import loader
+from django.http import HttpResponse, Http404  # 可以返回渲染的页面
+from django.shortcuts import render
 
 from .models import Meishi, Question  # 导入我们的模型类
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
