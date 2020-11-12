79b4dffe849e135c4b3b73fa7b7c2a303839c048
Bohemian-hub <dongshangwl@gmail.com>
2020-11-12 11:04:59 +0800

    创建了模板

--- xcc_gezi_project/polls/urls.py
+++ xcc_gezi_project/polls/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path("select_food/", views.select_food),
     path("update_food/", views.update_food),
     path("delete_food/", views.delete_food),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- xcc_gezi_project/polls/views.py
+++ xcc_gezi_project/polls/views.py
@@ -1,9 +1,28 @@
 from django.http import HttpResponse  # 可以返回渲染的页面
-from .models import Meishi  # 导入我们的模型类
+from django.template import loader
+
+from .models import Meishi, Question  # 导入我们的模型类
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
 
 # 添加数据方法
