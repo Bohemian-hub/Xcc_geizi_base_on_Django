c8899e34116aa4f997da04c323f203cfb08aec41
Bohemian-hub <dongshangwl@gmail.com>
2020-11-12 11:44:07 +0800

    完成了投票系统

--- xcc_gezi_project/polls/urls.py
+++ xcc_gezi_project/polls/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index),
+    path('', views.IndexView.as_view(), name='index'),
     path("add_food/", views.add_food),
     path("select_food/", views.select_food),
     path("update_food/", views.update_food),
     path("delete_food/", views.delete_food),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
